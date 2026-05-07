acfbe7724dc08f1c903f37ec933c4ccf6207e266
henryamenhauser <henry.amenhauser@gmail.com>
2026-05-07 10:05:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+public class Main {
+
+    public Card [] deck;
+    public int deckPosition;
+
+    public static void main(String[] args) {
+        Main blackjack = new Main();
+    }
+
+    public Main() {
+        deck = new Card[52];
+
+        for (int i =0; i<deck.length; i++) {
+            if (i<13) {
+                deck[i] = new Card(i % 13,"Spades",false);
+            } else if (i<26) {
+                deck[i] = new Card(i % 13,"Clubs",false);
+            } else if (i<39) {
+                deck[i] = new Card(i % 13,"Diamonds",false);
+            } else if (i<52) {
+                deck[i] = new Card(i % 13,"Hearts",false);
+            }
+        }
+
+        shuffle();
+        deckPosition = 0;
+
+        Player player = new Player();
+        Player dealer = new Player();
+
+        player.addCard(dealCard());
+        player.addCard(dealCard());
+
+        dealer.addCard(dealCard());
+
+        System.out.println("Player Hand:");
+        player.printHand();
+        System.out.println("Total: " + player.getTotal());
+
+        System.out.println();
+
+        System.out.println("Dealer Hand:");
+        dealer.printHand();
+        System.out.println("Total: " + dealer.getTotal());
+
+    }
+
+    public void printDeck() {
+        System.out.println("Deck:");
+        for (int i = 0; i < deck.length; i++) {
+            deck[i].printCard();
+        }
+    }
+
+    public void shuffle() {
+        for (int i = 0; i< deck.length; i++) {
+            int randNum = (int)(Math.random()*52);
+            Card holdCard = deck[randNum];
+            deck[randNum] = deck[i];
+            deck[i] = holdCard;
+        }
+    }
+
+    public Card dealCard(){
+        Card nextCard = deck[deckPosition];
+        deckPosition++;
+        return nextCard;
+    }
+
+}
---
+++ src/Player.java
@@ -0,0 +1,34 @@
+public class Player {
+
+    public Card[] hand;
+    public int numCards;
+
+    public Player() {
+        hand = new Card[12];
+        numCards = 0;
+
+    }
+
+    public void addCard(Card newCard) {
+        hand[numCards] = newCard;
+        numCards++;
+    }
+
+    public void printHand() {
+        for (int i = 0; i < numCards; i++) {
+            hand[i].printCard();
+        }
+    }
+
+    public int getTotal() {
+        int total = 0;
+        for (int i= 0; i < numCards; i++){
+            total += hand[i].geBlackjackValue();
+        }
+        return total;
+    }
+
+
+
+
+}
